4e937c8a7f857012601f55fb9a906527c554b9a9
Jonathan Benghiat <jonathan@phoboscap.fund>
2023-05-04 13:09:00 -0500

    Fix package setup bugs

---
+++ opensea-marketplace-indexer/tests/mapping.test.ts
@@ -0,0 +1,16 @@
+import { Address, BigInt, Bytes, ethereum } from "@graphprotocol/graph-ts";
+import { describe, test, newMockEvent, clearStore, assert, beforeAll, beforeEach, afterAll } from "matchstick-as/assembly/index"
+
+describe("handleOrdersMatched()", () => {
+    describe("When receipt is present", () => {
+        test("updates the entity", () => {
+
+        })
+    })
+
+    describe("When receipt is not present", () => {
+        test("it creates a new entity", () => {
+
+        })
+    })
+})
