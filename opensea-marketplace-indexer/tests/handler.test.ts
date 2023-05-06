372aadb4b29bc60c6a634f833b6b8403da9997dc
Jonathan Benghiat <jonathan@phoboscap.fund>
2023-05-06 12:32:20 -0500

    Add comments to handler tests

--- opensea-marketplace-indexer/tests/handler.test.ts
+++ opensea-marketplace-indexer/tests/handler.test.ts
@@ -17,10 +17,12 @@ const DUMMY_ADDRESS2 = "0x388c818ca8b9251b393131c08a736a67ccb19297"
 describe("getCollection()", () => {
     describe("when collection does not exist", () => {
         test("it returns ", () => {
+            // Ensure the collection doesn't exist prior to fetching
             const tokenAddress = DUMMY_ADDRESS
             const collection = store.get("Collection", tokenAddress)
             assert.assertNull(collection)
 
+            // Mock smart contract calls to control return values and execution flow
             createMockedFunction(Address.fromString(tokenAddress), "supportsInterface", "supportsInterface(bytes4):(bool)")
                 .withArgs([ethereum.Value.fromFixedBytes(Bytes.fromHexString(ERC721_INTERFACE_IDENTIFIER))])
                 .returns([ethereum.Value.fromBoolean(false)])
@@ -41,6 +43,7 @@ describe("getCollection()", () => {
                 .withArgs([])
                 .returns([ethereum.Value.fromI32(10)])
 
+            // Call function being tested, fetch collection from store directly, and ensure objects are the same by spot checking some fields
             const retVal = getCollection(tokenAddress)
             const expected = store.get("Collection", tokenAddress)
             assert.assertTrue(expected!.get("id")!.toString() == retVal.id)
@@ -54,6 +57,7 @@ describe("getCollection()", () => {
         test("it loads existing collection", () => {
             const tokenAddress = DUMMY_ADDRESS
 
+            // Mock smart contract calls
             createMockedFunction(Address.fromString(tokenAddress), "supportsInterface", "supportsInterface(bytes4):(bool)")
                 .withArgs([ethereum.Value.fromFixedBytes(Bytes.fromHexString(ERC721_INTERFACE_IDENTIFIER))])
                 .returns([ethereum.Value.fromBoolean(false)])
@@ -62,6 +66,7 @@ describe("getCollection()", () => {
                 .withArgs([ethereum.Value.fromFixedBytes(Bytes.fromHexString(ERC1155_INTERFACE_IDENTIFIER))])
                 .returns([ethereum.Value.fromBoolean(false)])
 
+            // Create collection object and persist to store so collection is there when getCollection is called
             const existingCollection = new Collection(tokenAddress);
             existingCollection.nftStandard = getNftStandard(tokenAddress);
             existingCollection.royaltyFee = BigDecimal.fromString("1.5");
@@ -74,9 +79,11 @@ describe("getCollection()", () => {
             existingCollection.sellerCount = 0;
             store.set("Collection", tokenAddress, existingCollection)
             
+            // Ensure collection is now there after persisting to store
             const collection = store.get("Collection", tokenAddress)
             assert.assertNotNull(collection)
 
+            // Call method being tested and compare returned collection with pre-existing one by spot checking fields
             const retVal = getCollection(tokenAddress)
             assert.assertTrue(retVal.id == existingCollection.id)
             assert.assertTrue(retVal.royaltyFee == existingCollection.royaltyFee)
@@ -85,6 +92,7 @@ describe("getCollection()", () => {
     })
 })
 
+// these test are synonymous with the getCollection tests, just with Marketplace objects
 describe("getMarketplace()", () => {
     describe("When marketplace does not exist", () => {
         test("it returns new market place", () => {
@@ -130,6 +138,7 @@ describe("getMarketplace()", () => {
     })
 })
 
+// these test are synonymous with the getCollection tests, just with MarketplaceDailySnapshot objects
 describe("getOrCreateMarketplaceDailySnapshot()", () => {
     describe("When market daily snapshot does not exist", () => {
         test("it returns new market daily snapshot place", () => {
@@ -179,6 +188,7 @@ describe("getOrCreateMarketplaceDailySnapshot()", () => {
     })
 })
 
+// these test are synonymous with the getCollection tests, just with CollectionDailySnapshot objects
 describe("getOrCreateCollectionDailySnapshot()", () => {
     describe("When collection daily snapshot does not exist", () => {
         test("it returns new collection daily snapshot place", () => {
@@ -235,6 +245,7 @@ describe("getNftStandard()", () => {
         test("it returns NFTStandards.UNKNOWN", () => {
             const collectionID = DUMMY_ADDRESS
 
+            // Mock smart contract calls
             createMockedFunction(Address.fromString(collectionID), "supportsInterface", "supportsInterface(bytes4):(bool)")
                 .withArgs([ethereum.Value.fromFixedBytes(Bytes.fromHexString(ERC721_INTERFACE_IDENTIFIER))])
                 .returns([ethereum.Value.fromBoolean(false)])
@@ -242,7 +253,7 @@ describe("getNftStandard()", () => {
             createMockedFunction(Address.fromString(collectionID), "supportsInterface", "supportsInterface(bytes4):(bool)")
                 .withArgs([ethereum.Value.fromFixedBytes(Bytes.fromHexString(ERC1155_INTERFACE_IDENTIFIER))])
                 .returns([ethereum.Value.fromBoolean(false)])
-            
+
             const retVal = getNftStandard(collectionID)
             assert.assertTrue(retVal == NFTStandards.UNKNOWN)
         })
@@ -252,6 +263,7 @@ describe("getNftStandard()", () => {
         test("it returns NFTStandards.ERC721", () => {
             const collectionID = DUMMY_ADDRESS
 
+            // Mock smart contract calls
             createMockedFunction(Address.fromString(collectionID), "supportsInterface", "supportsInterface(bytes4):(bool)")
                 .withArgs([ethereum.Value.fromFixedBytes(Bytes.fromHexString(ERC721_INTERFACE_IDENTIFIER))])
                 .returns([ethereum.Value.fromBoolean(true)])
@@ -269,6 +281,7 @@ describe("getNftStandard()", () => {
         test("it returns NFTStandards.ERC1155", () => {
             const collectionID = DUMMY_ADDRESS
 
+            // Mock smart contract calls
             createMockedFunction(Address.fromString(collectionID), "supportsInterface", "supportsInterface(bytes4):(bool)")
                 .withArgs([ethereum.Value.fromFixedBytes(Bytes.fromHexString(ERC721_INTERFACE_IDENTIFIER))])
                 .returns([ethereum.Value.fromBoolean(false)])
@@ -288,9 +301,11 @@ describe("getTransferDetails()", () => {
         test("it returns null", () => {
             const offerer = Address.fromString(DUMMY_ADDRESS)
             const recipient = Address.fromString(DUMMY_ADDRESS2)
-            
+
+            // Create empty offer
             const offer: OrderFulfilledOfferStruct[] = []
 
+            // Create non-empty consideration
             const considerationTupleArray = [
                 ethereum.Value.fromI32(SeaportItemType.NATIVE),
                 ethereum.Value.fromAddress(Address.fromString(DUMMY_ADDRESS2)),
@@ -313,6 +328,7 @@ describe("getTransferDetails()", () => {
             const offerer = Address.fromString(DUMMY_ADDRESS)
             const recipient = Address.fromString(DUMMY_ADDRESS2)
             
+            // Create non-empty offer
             const offerTupleArray = [
                 ethereum.Value.fromI32(SeaportItemType.NATIVE),
                 ethereum.Value.fromAddress(Address.fromString(DUMMY_ADDRESS)),
@@ -324,6 +340,7 @@ describe("getTransferDetails()", () => {
                 offerItem
             ]
 
+            // Create empty consideration
             const consideration: OrderFulfilledConsiderationStruct[] = []
 
             const retVal = getTransferDetails(offerer, recipient, offer, consideration)
@@ -336,6 +353,7 @@ describe("getTransferDetails()", () => {
             const offerer = Address.fromString(DUMMY_ADDRESS)
             const recipient = Address.fromString(DUMMY_ADDRESS2)
             
+            // Create non-empty offer that consists of money and not NFTs
             const offerTupleArray = [
                 ethereum.Value.fromI32(SeaportItemType.NATIVE),
                 ethereum.Value.fromAddress(Address.fromString(DUMMY_ADDRESS)),
@@ -347,6 +365,7 @@ describe("getTransferDetails()", () => {
                 offerItem
             ]
 
+            // Create non-empty consideration that consists of money and not NFTs
             const considerationTupleArray = [
                 ethereum.Value.fromI32(SeaportItemType.ERC20),
                 ethereum.Value.fromAddress(Address.fromString(DUMMY_ADDRESS2)),
@@ -369,6 +388,7 @@ describe("getTransferDetails()", () => {
             const offerer = Address.fromString(DUMMY_ADDRESS)
             const recipient = Address.fromString(DUMMY_ADDRESS2)
             
+            // Create non-empty offer that consists of money and not NFTs
             const offerTupleArray = [
                 ethereum.Value.fromI32(SeaportItemType.NATIVE),
                 ethereum.Value.fromAddress(Address.fromString(DUMMY_ADDRESS)),
@@ -380,6 +400,7 @@ describe("getTransferDetails()", () => {
                 offerItem
             ]
 
+            // Create non-empty consideration that consists of NFTs and not money
             const considerationTupleArray = [
                 ethereum.Value.fromI32(SeaportItemType.ERC721),
                 ethereum.Value.fromAddress(Address.fromString(DUMMY_ADDRESS2)),
@@ -404,6 +425,7 @@ describe("getTransferDetails()", () => {
             const offerer = Address.fromString(DUMMY_ADDRESS)
             const recipient = Address.fromString(DUMMY_ADDRESS2)
             
+            // Create non-empty offer that consists of NFTs
             const offerTupleArray = [
                 ethereum.Value.fromI32(SeaportItemType.ERC1155),
                 ethereum.Value.fromAddress(Address.fromString(DUMMY_ADDRESS)),
@@ -415,6 +437,7 @@ describe("getTransferDetails()", () => {
                 offerItem
             ]
 
+            // Create non-empty consideration that consists of NFTs
             const considerationTupleArray = [
                 ethereum.Value.fromI32(SeaportItemType.ERC721),
                 ethereum.Value.fromAddress(Address.fromString(DUMMY_ADDRESS2)),
@@ -437,6 +460,7 @@ describe("getTransferDetails()", () => {
             const offerer = Address.fromString(DUMMY_ADDRESS)
             const recipient = Address.fromString(DUMMY_ADDRESS2)
             
+            // Create non-empty offer that consists of money
             const offerTupleArray = [
                 ethereum.Value.fromI32(SeaportItemType.NATIVE),
                 ethereum.Value.fromAddress(Address.fromString(DUMMY_ADDRESS)),
@@ -448,6 +472,7 @@ describe("getTransferDetails()", () => {
                 offerItem
             ]
 
+            // Create non-empty consideration that consists of money
             const considerationTupleArray = [
                 ethereum.Value.fromI32(SeaportItemType.NATIVE),
                 ethereum.Value.fromAddress(Address.fromString(DUMMY_ADDRESS2)),
@@ -470,6 +495,7 @@ describe("getTransferDetails()", () => {
             const offerer = Address.fromString(DUMMY_ADDRESS)
             const recipient = Address.fromString(DUMMY_ADDRESS2)
             
+            // Create non-empty offer that consists of NFTs
             const offerTupleArray = [
                 ethereum.Value.fromI32(SeaportItemType.ERC721),
                 ethereum.Value.fromAddress(Address.fromString(DUMMY_ADDRESS)),
@@ -481,6 +507,7 @@ describe("getTransferDetails()", () => {
                 offerItem
             ]
 
+            // Create non-empty consideration that consists of moeny
             const considerationTupleArray = [
                 ethereum.Value.fromI32(SeaportItemType.NATIVE),
                 ethereum.Value.fromAddress(Address.fromString(DUMMY_ADDRESS2)),
@@ -504,6 +531,7 @@ describe("getTransferDetails()", () => {
 describe("extractMoneyFromConsideration()", () => {
     describe("when all money in consideration sums to zero", () => {
         test("it returns null", () => {
+            // Create consideration
             const tupleArray = [
                 ethereum.Value.fromI32(SeaportItemType.NATIVE),
                 ethereum.Value.fromAddress(Address.fromString(DUMMY_ADDRESS)),
@@ -523,6 +551,7 @@ describe("extractMoneyFromConsideration()", () => {
 
     describe("when all money in consideration sums to non-zero", () => {
         test("it returns correct money amount", () => {
+            // Create consideration
             const tupleArray1 = [
                 ethereum.Value.fromI32(SeaportItemType.NATIVE),
                 ethereum.Value.fromAddress(Address.fromString(DUMMY_ADDRESS)),
@@ -551,6 +580,7 @@ describe("extractMoneyFromConsideration()", () => {
 
     describe("when consideration is all non-money", () => {
         test("it returns null", () => {
+            // Create consideration
             const tupleArray1 = [
                 ethereum.Value.fromI32(SeaportItemType.ERC1155),
                 ethereum.Value.fromAddress(Address.fromString(DUMMY_ADDRESS)),
@@ -570,6 +600,7 @@ describe("extractMoneyFromConsideration()", () => {
 
     describe("when consideration is partially money and partially non-money and sums to zero", () => {
         test("it returns null", () => {
+            // Create consideration
             const tupleArray1 = [
                 ethereum.Value.fromI32(SeaportItemType.NATIVE),
                 ethereum.Value.fromAddress(Address.fromString(DUMMY_ADDRESS)),
@@ -598,6 +629,7 @@ describe("extractMoneyFromConsideration()", () => {
 
     describe("when consideration is partially money and partially non-money and sums to non-zero", () => {
         test("it returns sum of money consideration items only", () => {
+            // Create consideration
             const tupleArray1 = [
                 ethereum.Value.fromI32(SeaportItemType.NATIVE),
                 ethereum.Value.fromAddress(Address.fromString(DUMMY_ADDRESS)),
@@ -628,6 +660,7 @@ describe("extractMoneyFromConsideration()", () => {
 describe("extractNFTsFromOffer()", () => {
     describe("when all offer items are non-NFTs", () => {
         test("it returns null", () => {
+            // Create offer
             const tupleArray = [
                 ethereum.Value.fromI32(SeaportItemType.NATIVE),
                 ethereum.Value.fromAddress(Address.fromString(DUMMY_ADDRESS)),
@@ -646,6 +679,7 @@ describe("extractNFTsFromOffer()", () => {
 
     describe("when offer contains multiple NFTs and are different tokens", () => {
         test("it returns null", () => {
+            // Create offer
             const tupleArray1 = [
                 ethereum.Value.fromI32(SeaportItemType.ERC721),
                 ethereum.Value.fromAddress(Address.fromString(DUMMY_ADDRESS)),
@@ -672,6 +706,7 @@ describe("extractNFTsFromOffer()", () => {
 
     describe("when offer contains multiple NFTs and are same tokens that are ERC721", () => {
         test("it returns an ERC721 NFTs collection", () => {
+            // Create offer
             const tupleArray1 = [
                 ethereum.Value.fromI32(SeaportItemType.ERC721),
                 ethereum.Value.fromAddress(Address.fromString(DUMMY_ADDRESS)),
@@ -709,6 +744,7 @@ describe("extractNFTsFromOffer()", () => {
 
     describe("when offer contains multiple NFTs and are same tokens that are ERC1155", () => {
         test("it returns an ERC1155 NFTs collection", () => {
+            // Create offer
             const tupleArray1 = [
                 ethereum.Value.fromI32(SeaportItemType.ERC1155),
                 ethereum.Value.fromAddress(Address.fromString(DUMMY_ADDRESS)),
@@ -748,6 +784,7 @@ describe("extractNFTsFromOffer()", () => {
 describe("extractNFTsFromConsideration()", () => {
     describe("when all consideration items are non-NFTs", () => {
         test("it returns null", () => {
+            // Create consideration
             const tupleArray = [
                 ethereum.Value.fromI32(SeaportItemType.NATIVE),
                 ethereum.Value.fromAddress(Address.fromString(DUMMY_ADDRESS)),
@@ -767,6 +804,7 @@ describe("extractNFTsFromConsideration()", () => {
 
     describe("when consideration contains multiple NFTs and are different tokens", () => {
         test("it returns null", () => {
+            // Create consideration
             const tupleArray1 = [
                 ethereum.Value.fromI32(SeaportItemType.ERC721),
                 ethereum.Value.fromAddress(Address.fromString(DUMMY_ADDRESS)),
@@ -795,6 +833,7 @@ describe("extractNFTsFromConsideration()", () => {
 
     describe("when consideration contains multiple NFTs and are same tokens that are ERC721", () => {
         test("it returns an ERC721 NFTs collection", () => {
+            // Create consideration
             const tupleArray1 = [
                 ethereum.Value.fromI32(SeaportItemType.ERC721),
                 ethereum.Value.fromAddress(Address.fromString(DUMMY_ADDRESS)),
@@ -834,6 +873,7 @@ describe("extractNFTsFromConsideration()", () => {
 
     describe("when consideration contains multiple NFTs and are same tokens that are ERC1155", () => {
         test("it returns an ERC1155 NFTs collection", () => {
+            // Create consideration
             const tupleArray1 = [
                 ethereum.Value.fromI32(SeaportItemType.ERC1155),
                 ethereum.Value.fromAddress(Address.fromString(DUMMY_ADDRESS)),
@@ -873,6 +913,7 @@ describe("extractNFTsFromConsideration()", () => {
 describe("extractOpenSeaRoyaltyFees()", () => {
     describe("when consideration does not have open sea fee account recipient and has only excluded recipient items", () => {
         test("it returns zero fees", () => {
+            // Create consideration
             const tupleArray1 = [
                 ethereum.Value.fromI32(SeaportItemType.NATIVE),
                 ethereum.Value.fromAddress(Address.fromString(DUMMY_ADDRESS)),
@@ -902,6 +943,7 @@ describe("extractOpenSeaRoyaltyFees()", () => {
 
     describe("when consideration does have open sea fee account recipient and has only excluded recipient items", () => {
         test("it returns non-zero protocol fees and zero royalty fees", () => {
+            // Create consideration
             const tupleArray1 = [
                 ethereum.Value.fromI32(SeaportItemType.NATIVE),
                 ethereum.Value.fromAddress(Address.fromString(DUMMY_ADDRESS)),
@@ -931,6 +973,7 @@ describe("extractOpenSeaRoyaltyFees()", () => {
 
     describe("when consideration does not have open sea fee account recipient and has non-excluded recipient items", () => {
         test("it returns zero protocol fees and non-zero royalty fees", () => {
+            // Create consideration
             const tupleArray1 = [
                 ethereum.Value.fromI32(SeaportItemType.NATIVE),
                 ethereum.Value.fromAddress(Address.fromString(DUMMY_ADDRESS)),
@@ -960,6 +1003,7 @@ describe("extractOpenSeaRoyaltyFees()", () => {
 
     describe("when consideration does have open sea fee account recipient and has non-excluded recipient items", () => {
         test("it returns non-zero protocol fees and non-zero royalty fees", () => {
+            // Create consideration
             const tupleArray1 = [
                 ethereum.Value.fromI32(SeaportItemType.NATIVE),
                 ethereum.Value.fromAddress(Address.fromString(DUMMY_ADDRESS)),
